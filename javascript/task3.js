fca786d81d28f119babed345070a6ddfec75f022
sangeetha <sangeetha.sakthimurugan@crystaldelta.com>
2025-07-24 11:27:51 +0530

    created task/js folder

---
+++ javascript/task3.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    const firstname = document.getElementById("firstname").value.trim();
+    const secondname = document.getElementById("secondname").value.trim();
+    const number = document.getElementById("contactNumber").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const location = document.getElementById("location").value.trim();
+
+    if (firstname === "" || secondname === "" || number === "" || email === "" || location === "") {
+        document.getElementById("error-messages").style.display = "block";
+    } else {
+        document.getElementById("error-messages").style.display = "none";
+    }
+}
